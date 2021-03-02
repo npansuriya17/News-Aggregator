499a18d4676021c8131824cd08e271693ea1529b
npansuriya17 <73963997+npansuriya17@users.noreply.github.com>
2021-03-02 10:44:59 -0600

    Update settings.py

--- NewsAggregator/settings.py
+++ NewsAggregator/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7ngt)ahgy4^0cjb#xi(v8!3s9hhcw_f-v!yrweq0tdvj+w-o#h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
